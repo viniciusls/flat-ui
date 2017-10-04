d2d1dcd89da576d984b57f40b678f248acd0bed5
Vinícius Silva <vinicius768@msn.com>
2017-10-04 11:42:43 -0300

    Added dist and src folders + css/js folders inside each; Fixed gulpfile to match directory structure

--- gulpfile.js
+++ gulpfile.js
@@ -9,60 +9,53 @@ var rename = require('gulp-rename');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 
-
 gulp.task('clean', function () {
-	return gulp.src('public/assets/dist/')
-		.pipe(clean());
+	return gulp.src('dist/')
+		.pipe(clean())
 });
 
 gulp.task('sass', function () {
-	gulp.src('resources/assets/sass/*.scss')
+	gulp.src('src/scss/*.scss')
 		.pipe(sass().on('error', sass.logError))
 		.pipe(autoprefixer({
 			browsers: ['last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4']
 		}))
-		.pipe(gulp.dest('resources/assets/css'));
+		.pipe(gulp.dest('resources/assets/css'))
 });
 
 gulp.task('jshint',function () {
- return gulp.src(['resources/assets/js/*.js', 'resources/assets/js/modules/*.js'])
- .pipe(jshint())
- .pipe(jshint.reporter('default'));
+	return gulp.src(['src/js/*.js'])
+ 		.pipe(jshint())
+ 		.pipe(jshint.reporter('default'))
 });
 
 gulp.task('uglify', function () {
-	gulp.src(['resources/assets/js/*.js']).pipe(uglify())
+	gulp.src(['src/js/*.js']).pipe(uglify())
 		.pipe(rename({
 			suffix: '.min'
 		}))
-		.pipe(gulp.dest('public/assets/dist/js'));
-
-	gulp.src(['resources/assets/js/modules/*.js'])
-		.pipe(gulp.dest('public/assets/dist/js/modules'));
+		.pipe(gulp.dest('dist/js'))
 });
 
 gulp.task('cssmin', function() {
-	gulp.src('resources/assets/css/*.css')
+	gulp.src('dist/css/*.css')
 		.pipe(cleanCSS())
 		.pipe(rename({
 			suffix: '.min'
 		}))
-		.pipe(gulp.dest('public/assets/dist/css'));
+		.pipe(gulp.dest('dist/css'))
 
 });
 
 gulp.task('watch', function() {
-	gulp.watch('resources/assets/sass/*.scss', ['sass']).on('change', function(event) {
+	gulp.watch('src/scss/*.scss', ['sass']).on('change', function(event) {
 		console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
-	});
-	gulp.watch('/*.html',['htmlmin']);
-	gulp.watch('resources/assets/js/*.js',['uglify']);
-	gulp.watch('resources/assets/js/modules/*.js',['uglify']);
-	gulp.watch('resources/assets/css/*.css',['cssmin']);
+	})
+	gulp.watch('src/js/*.js',['jshint', 'uglify'])
+	gulp.watch('dist/css/*.css',['cssmin'])
 });
 
 gulp.task('default', function (callback) {
-	//return runSequence('clean',['jshint','htmlmin','sass','uglify','watch'],callback)
-	return runSequence('clean',['jshint', 'sass','uglify','cssmin','watch'],callback)
+	return runSequence('clean',['jshint', 'sass', 'uglify', 'cssmin', 'watch'], callback)
 });
 
